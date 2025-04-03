c264534789224c2b2371b68190cd86d45b0a17bd
RM <rashimishr29@gmail.com>
2025-04-03 12:31:04 +0530

    mapper

--- Brewserve.Core/Mapping/AutoMapperProfile.cs
+++ Brewserve.Core/Mapping/AutoMapperProfile.cs
@@ -13,13 +13,6 @@ namespace BrewServe.Core.Mapping
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.BarBeers, opt => opt.Ignore()) // Ignore if it's not part of CreateBarRequest
                 .ReverseMap();
-            //CreateMap<Bar, BarRequest>()
-            //    .ForMember(dest => dest.Beers, opt => opt.MapFrom(src => src.BarBeers.Select(bb => new BeerRequest
-            //    {
-            //        Id = bb.Beer.Id,
-            //        Name = bb.Beer.Name,
-            //        PercentageAlcoholByVolume = bb.Beer.PercentageAlcoholByVolume,
-            //    }).ToList()));
             CreateMap<Bar, BarResponse>().ReverseMap();
 
             // Beer Mappings
@@ -29,12 +22,6 @@ namespace BrewServe.Core.Mapping
                 .ForMember(dest => dest.BreweryBeers,
                     opt => opt.Ignore()) // Ignore if it's not part of CreateBeerRequest
                 .ReverseMap();
-            //CreateMap<Beer, BeerRequest>()
-            //    .ForMember(dest => dest.Breweries, opt => opt.MapFrom(src => src.BreweryBeers.Select(bb => new BreweryRequest
-            //    {
-            //        Id = bb.Brewery.Id,
-            //        Name = bb.Brewery.Name,
-            //    }).ToList()));
             CreateMap<Beer, BeerResponse>().ReverseMap();
 
             // Brewery Mappings
@@ -42,22 +29,12 @@ namespace BrewServe.Core.Mapping
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.BreweryBeers, opt => opt.Ignore()) //Ignore if it's not part 
                 .ReverseMap();
-            //CreateMap<Brewery, BreweryRequest>()
-            //    .ForMember(dest => dest.Beers, opt => opt.MapFrom(src => src.BreweryBeers.Select(bb => new BeerRequest
-            //    {
-            //        Id = bb.Beer.Id,
-            //        Name = bb.Beer.Name,
-            //        PercentageAlcoholByVolume = bb.Beer.PercentageAlcoholByVolume,
-            //    }).ToList()))
-            //    .ReverseMap();
             CreateMap<Brewery, BreweryResponse>();
 
             // BarBeerLink Mappings
             CreateMap<BarBeerLink, BarBeerLinkRequest>();
             // BreweryBeerLink Mappings
             CreateMap<BreweryBeerLink, BreweryBeerLinkRequest>();
-
-
         }
     }
 }
